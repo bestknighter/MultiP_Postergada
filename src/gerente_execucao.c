6f41558e19bad2f667b338bcf147ac576e145320
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2019-05-28 15:56:39 -0300

    Começa a instanciação das topologias

--- src/gerente_execucao.c
+++ src/gerente_execucao.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <sys/types.h>
 
 #include "gerente_execucao.h"
 
+extern key_t escalonadorMsqKey;
+extern int escalonadorMsqID;
+
 time_t start, end;
-pid_t proc, me;
+pid_t proc;
+gerente_init_t dados;
 
 void exec() {
 	if( 0 == (proc = fork()) ) {
@@ -20,8 +24,50 @@ void end_exec() {
 	// avisa escalonador que terminou com tempo start e end
 }
 
-void gerente_loop() {
-	me = getpid();
+gerente_init_t* cria_gerentes( int topologia ) {
+	if( HYPERCUBE > topologia || FAT_TREE < topologia ) {
+		fprintf( stderr, "[ERRO] Topologia desconhecida!\nExistentes: 0 - Hypercubo, 1 - Torus, 2 - Fat Tree.\nFornecida: %d\n\n", topologia );
+		return NULL;
+	}
+
+	gerente_init_t* gerentes;
+
+	gerentes = (gerente_init_t*) calloc(16, sizeof(gerente_init_t));
+
+	gerentes[0]->escalonador.pid = getpid();
+	gerentes[0]->escalonador.msqKey = escalonadorMsqKey;
+	gerentes[0]->escalonador.msqID = escalonadorMsqID;
+
+	for( int i = 0; i < 16; i++ ) {
+		gerentes[i].self.id = i;
+		gerentes[i].self.msqKey = 0x1233 + i;
+		gerentes[i].self.msqID = msgget( gerentes[i].self.msqKey, IPC_CREAT|0x1ff );
+		if( 0 > gerentes[i].self.msqID ) {
+			fprintf( stderr, "[ERRO] Erro na obtencao de fila para o gerente %d.\n\n", i );
+			free( gerentes );
+			return NULL;
+		}
+	}
+
+	if( HYPERCUBE == topologia ) {
+		// Como conectar um hypercube??
+	} else if( TORUS == topologia ) {
+		for( int i = 0; i < 16; i++ ) {
+			gerentes[i].noVizinho[0] = gerentes[ (i+12)%16 ].self; // UP
+			gerentes[i].noVizinho[1] = gerentes[ ((i%4)+1)%4 + 4*((int)i/4) ].self; // RIGHT
+			gerentes[i].noVizinho[2] = gerentes[ (i+4)%16 ].self; // DOWN
+			gerentes[i].noVizinho[3] = gerentes[ ((i%4)+3)%4 + 4*((int)i/4) ].self; // LEFT
+		}
+	} else { // Fat Tree
+		// Como conectar um Fat Tree??
+	}
+
+	return gerentes;
+}
+
+void gerente_loop( gerente_init_t dadosIniciais) {
+	dados = dadosIniciais;
+	dados.self.pid = getpid();
 	while( 1 ) {
 		// verifica se tem msgs pra receber, se sim
 			// se tiver que encaminhar alguma msg, faça
