43d327caa359d9ee86834eb7bde448269783e4c9
Cristiane Naves <cristianenavescardoso09@gmail.com>
2019-06-03 16:19:43 -0300

    documentando

--- include/escalonador.h
+++ include/escalonador.h
@@ -1,4 +1,3 @@
-/* Semaforo implementado por Alba Melo. */
 #ifndef ESCALONADOR_H
 #define ESCALONADOR_H
 
@@ -12,38 +11,13 @@
 #include <unistd.h>
 #include <sys/msg.h>
 #include <signal.h>
-#include <time.h>
-
-#include "../include/gerente_execucao.h"
-
-// Declare the message structure.
-#define MSGSZ 128
-
-int topologia;
-
-typedef struct msgbuf {
-	long mtype;
-	char mtext[MSGSZ];
-} message_buf;
-
-message_buf rbuf;
-
-typedef struct msgTerminoGerentes {
-	long mtype;
-	char mtext[64];
-} msg_termino_gerentes;
-
-msg_termino_gerentes msg_termino;
 
 struct sembuf operacao[2];
-int idsem;
-
-void executa_programa();
 
 int p_sem();
 int v_sem();
 
-char * buscar_info(int *tempoEspera, int *jobID, char *nomePrograma);
+char * buscar_info(int *tempoEspera, int *jobID);
 double esperar_mensagens();
 double tempo_execucao(char *msg_termino);
 
--- src/escalonador.c
+++ src/escalonador.c
@@ -4,23 +4,28 @@
 #include <string.h>
 #include <time.h>
 
-key_t escalonadorMsqKey = 0x1123;
-int escalonadorMsqID;
+key_t escalonadorMsqKey = 0x1123; /**< Variavel global que vai ser usado pelo gerente_execucao como extern */
+int escalonadorMsqID; /**< Variavel global que tambem vai ser usada por gerente_execucao */
 
+#define MSGSZ 128
+
+int topologia; /**< Variavel que vai guardar o valor da topologia passada como parametro para o escalonador */
+int idsem;
 /**
- * @brief Buffer de mensagens entre gerentes de processo
+ * @brief Buffer de mensagens entre gerentes de processo e entre
+ * executa_postergado
  */
 struct msgBuf {
 	long mtype; /**< Tipo da mensagem, podendo ser MSG_END */
-	char mtext[64]; /**< Conteúdo da mensagem */
-} msg; // Já cria uma variável global desse tipo
+	char mtext[MSGSZ]; /**< Conteúdo da mensagem */
+} msgGerente, msgPost; // Já cria uma variável global desse tipo
 
 int main( int argc, char *argv[ ] ) {
 	int executaPostergadoMsqID;
-	key_t key;
+	key_t keyMsgPost;
 	topologia = atoi(argv[1]);
 
-	key = 2234;
+	keyMsgPost = 2234;
 
 	if((escalonadorMsqID = msgget(escalonadorMsqKey, IPC_CREAT|0x1ff)) < 0) {
 		perror("msgget escalonador");
@@ -28,7 +33,7 @@ int main( int argc, char *argv[ ] ) {
 	}
 
 	/*busca a fila de mensagem do executa postergado*/
-	if ((executaPostergadoMsqID = msgget(key, 0666)) < 0) {   //não pode executar antes de criar a fila, ou seja, antes do executa_postergado
+	if ((executaPostergadoMsqID = msgget(keyMsgPost, 0666)) < 0) {   //não pode executar antes de criar a fila, ou seja, antes do executa_postergado
 		perror("msgget");
 		exit(1);
 	}
@@ -42,7 +47,7 @@ int main( int argc, char *argv[ ] ) {
 
     while (1) {
  		/*Receive an answer of message type 1.*/
-		if (msgrcv(executaPostergadoMsqID, &rbuf, MSGSZ, 1, 0) < 0) {
+		if (msgrcv(executaPostergadoMsqID, &msgPost, sizeof(msgPost.mtext), 1, 0) < 0) {
 			perror("msgrcv");
 			exit(1);
 		}
@@ -51,35 +56,48 @@ int main( int argc, char *argv[ ] ) {
 		int jobID;
 		char *nomePrograma;
 
-		nomePrograma=buscar_info(&tempoEspera, &jobID, nomePrograma);
+		nomePrograma=buscar_info(&tempoEspera, &jobID);
+
+        printf("tempoEspera: %d\n", tempoEspera);
+        printf("jobID: %d\n", jobID);
+        printf("nomePrograma: %s\n", nomePrograma);
+
 		sleep(tempoEspera);
-		//p_sem();
+		p_sem();
 			executar_programa(gerentes_execucao[0].self.msqID, jobID, nomePrograma);
 			double makespan = esperar_mensagens();	
 			printf("job=%d, arquivo=%s, delay=%d segundos, makespan=%f segundos\n", jobID, nomePrograma, tempoEspera, makespan);	
-		//v_sem();
+		v_sem();
 
 	}
 	exit(0);
 }
 
-/*retorna makespan*/
+/**
+ * @brief Espera a mensagem de termino de todos os processos gerentes
+ * e calcula o makespan desses processos.
+ * @return retorna o turnaround(makespan) em segundos  
+ */
 double esperar_mensagens() {
 	double makespan = 0.0;
 	int total_proc = (topologia == HYPERCUBE || topologia == TORUS) ? 16 : 15;
 
 	for(int i=0; i < total_proc; i++) {
-		if(msgrcv(escalonadorMsqID, &msg, sizeof(msg.mtext), MSG_END, 0) < 0) {
+		if(msgrcv(escalonadorMsqID, &msgGerente, sizeof(msgGerente.mtext), MSG_END, 0) < 0) {
 			perror("msgrcv mensagem termino");
 			exit(1);
 		}
-		makespan += tempo_execucao(msg.mtext);
+		makespan += tempo_execucao(msgGerente.mtext);
 	}
 	return makespan;	
 }
 
-/*busca os dados da mensagem recebida de um proc. ger. e calcula a 
-  diferenca de tempo do inicio e do fim da execucao.*/
+/**
+ * @brief Calcula o tempo de execucao de um processo
+ * 
+ * @param[in] msg possuindo o formato: int_jobID int_gerenteID time_t_start time_t_end
+ * @return diferenca de tempo entre end e start
+ */
 double tempo_execucao(char *msg) {
 	printf("msg before: %s\n", msg);
 	char delim[] = " ";
@@ -101,15 +119,21 @@ double tempo_execucao(char *msg) {
 	return difftime(end, start);
 }
 
-/*Busca o delay, o jobID e o nome do programa do char* recebido 
-  da mensagem do executa_postergado*/
-char * buscar_info(int *tempoEspera, int *jobID, char *nomePrograma) {
+/**
+ * @brief Busca o delay, o jobID e o nomePrograma da mensagem recebida
+ * do executa_postergado
+ * 
+ * @param[in] tempoEspera recebe o endereco da variavel que vai guardar o delay
+ * @param[in] jobID recebe o endereco da variavel que vai guardar o job
+ * @return char * nomePrograma 
+ */
+char * buscar_info(int *tempoEspera, int *jobID) {
 	char delim[] = " ";
 
-	char *ptr = strtok(rbuf.mtext, delim);
+	char *ptr = strtok(msgPost.mtext, delim);
 	*jobID = atoi(ptr);
 
-	nomePrograma = strtok(NULL, delim);
+	char *nomePrograma = strtok(NULL, delim);
 
 	ptr = strtok(NULL, delim);
 	*tempoEspera = atoi(ptr);
@@ -117,14 +141,16 @@ char * buscar_info(int *tempoEspera, int *jobID, char *nomePrograma) {
    return nomePrograma;
 }
 
+/* Obs.: nao esta sendo usada, funcao feita para debugar o codigo. */
 char * time_to_string(time_t time) {
-	 char buff[20];
+	char buff[20];
 	struct tm * timeinfo;
 	timeinfo = localtime (&time);
 	strftime(buff, sizeof(buff), "%b %d %H:%M:%S", timeinfo);
 	return buff;
 }
 
+/* Semaforo implementado por Alba Melo. */
 int p_sem() {
     operacao[0].sem_num = 0;
     operacao[0].sem_op = 0;
